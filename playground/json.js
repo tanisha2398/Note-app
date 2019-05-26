dd5b2a704b8789c58d6b9f197549a97b1a9d0fb4
tanisha2398 <tanishanegipro@gmail.com>
2019-05-27 01:08:01 +0530

    add note and read note using JSON and using fs module storing the JSON string in notes.json file

--- playground/json.js
+++ playground/json.js
@@ -5,7 +5,23 @@
 // console.log(typeof stringObj);
 // console.log(stringObj);
 
-var personString = '{"name": "Tanisha","age": 25}';
-var person = JSON.parse(personString);
-console.log(typeof person);
-console.log(person);
+// var personString = '{"name": "Tanisha","age": 25}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require('fs');
+//Add new note
+var originalNote = {
+    title: 'Some Title',
+    body: 'some Body'
+};
+var originalNoteString=JSON.stringify(originalNote);
+fs.writeFileSync('notes.json',originalNoteString);
+console.log(typeof originalNoteString);
+console.log(originalNoteString);
+//Read new note 
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+console.log(typeof note);
+ console.log(note);
