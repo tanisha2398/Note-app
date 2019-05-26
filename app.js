fb361718ac11722b446917595eb05ff475297eff
tanisha2398 <tanishanegipro@gmail.com>
2019-05-26 23:09:20 +0530

    using yargs to parse argument

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 console.log("starting app.js ");
 
 const fs = require('fs');
+const yargs = require('yargs');
 const notes = require('./notes.js');
 const _ = require('lodash');
+
+
+const argv = yargs.argv;
 var command = process.argv[2];
-console.log(process.argv);
+
+
+
+console.log('yargs : ',argv);
 console.log("command : " ,command);
+
+
 if(command === 'add'){
-    console.log("add the note");
+    // console.log("add the note");
+    notes.addNote(argv.title,argv.body);
 }else if(command === 'list'){
-    console.log('listing all notes');
+    notes.getAll();
 }else if(command === 'read'){
-    console.log('reading individual note');
+    notes.readNote(argv.title);
 }else if(command === 'remove'){
-    console.log('removing note');
+    notes.removeNote(argv.title);
 }else{
     console.log('command not recognized')
 }
--- notes.js
+++ notes.js
@@ -1,6 +1,22 @@
 console.log('staring notes.js');
-module.exports.addNote= () => {
-    console.log('addNote');
-    return 'hi new note@@'
+var addNote =(title,body) => {
+    console.log('adding note',title,body);
+};
+var getAll =()=>{
+    console.log('getting all notes');
 };
 
+var readNote =(title) => {
+    console.log('reading note',title);
+};
+
+var removeNote =(title) => {
+    console.log('removing note',title);
+};
+
+module.exports={
+    addNote,
+    getAll,
+    readNote,
+    removeNote
+};
