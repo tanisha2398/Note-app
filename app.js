e605a301b822c2353b2a55b4af0a20c48f0e0f0a
tanisha2398 <tanishanegipro@gmail.com>
2019-05-26 20:21:02 +0530

    using require fn for own created module

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-console.log("starting app");
+console.log("starting app.js ");
 
 const fs = require('fs');
 const os = require('os');
-var user = os.userInfo();
+// var user = os.userInfo();
+const notes = require('./notes.js');
+// var res = notes.addNote();
+// console.log(res);
+var sum = notes.add(-9,10);
+console.log(sum);
 //console.log(user);
-fs.appendFileSync('greetings.txt',`Hello ${user.username}!`);
+// fs.appendFileSync('greetings.txt',`Hello ${user.username}! You are ${notes.age}.` );
---
+++ notes.js
@@ -0,0 +1,12 @@
+console.log('staring notes.js');
+// module.exports.addNote= () => {
+//     console.log('addNote');
+//     return 'hi new note@@'
+// };
+
+module.exports.add=(a,b) => {
+    console.log("add fn called");
+    return a+b;
+
+}
+// console.log(module);
