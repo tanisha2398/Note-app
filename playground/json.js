650a4dce43489fd89a78109f56f6649f047543c5
tanisha2398 <tanishanegipro@gmail.com>
2019-05-26 23:28:42 +0530

    using json to convert string into object

--- playground/json.js
+++ playground/json.js
@@ -1,6 +1,11 @@
-var obj = {
-    name: 'Tanisha'
-};
-var stringObj = JSON.stringify(obj);
-console.log(typeof stringObj);
-console.log(stringObj);
+// var obj = {
+//     name: 'Tanisha'
+// };
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+var personString = '{"name": "Tanisha","age": 25}';
+var person = JSON.parse(personString);
+console.log(typeof person);
+console.log(person);
